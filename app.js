6d4f7ced27dc83b4590fe002f66c181a7ad5228c
Ashley Dragan <ashleydragan@Ashleys-MacBook-Air.local>
2021-03-05 16:43:04 -0500

    added in solutions

--- app.js
+++ app.js
@@ -1,196 +1,27 @@
-// // FUNCTION DECLARATION
-// // function greet() {
-// // 	console.log('HI!');
-// // }
 
-// // FUNCTION EXPRESSION
-// // const add = function(x, y) {
-// // 	return x + y;
-// // };
-
-// // FUNCTION EXPRESSION (passed as arg to map)
-// // array.map(function(x){
-// // 	return x * 2
-// // })
-
-// // Arrow functions only work as function expressions:
-// const add = (x, y) => {
-// 	return x + y;
-// };
-
-// // Using a regular function expression:
-// [ 2, 3, 6, 78, 99, 104, 23 ].reduce(function(max, currNum) {
-// 	return Math.max(max, currNum);
-// });
-
-// // Using an arrow function expression:
-// [ 2, 3, 6, 78, 99, 104, 23 ].reduce((max, currNum) => {
-// 	return Math.max(max, currNum);
-// });
-
-// // *****************************
-// // ARROW FUNCTION "SHORTCUTS"
-// // *****************************
-
-// // [ 1, 2, 3, 4, 5 ].forEach(n => {
-// // 	console.log(n * 10);
-// // });
-
-// // const greet = () => {
-// // 	console.log('HELLO!!!');
-// // };
-
-// [ 1, 2, 3, 4, 5, 6 ].filter(function(num) {
-// 	return num % 2 === 0;
-// });
-
-// [ 1, 2, 3, 4, 5, 6 ].filter((num) => num % 2 === 0);
-
-// const double = (n) => n * 2;
-
-// [ 1, 5, 7, 9, 10, 12, 13, 15 ].map((n) => {
-// 	if (n % 2 === 0) {
-// 		return 'even';
-// 	}
-// 	return 'odd';
-// });
-
-// // [ 1, 5, 7, 9, 10, 12, 13, 15 ].map((n) =>
-// // 	if (n % 2 === 0) {
-// // 		 'even';
-// // 	}
-// // 	 'odd';
-// // );
-// [ 1, 5, 7, 9, 10, 12, 13, 15 ].map((n) => (n % 2 === 0 ? 'even' : 'odd'));
-
-// const dailyRainTotals = [
-// 	[ 1.2, 0.35, 2.2 ],
-// 	[ 1.7, 0.6, 0.1 ],
-// 	[ 2.5, 0.9, 1.5 ]
-// ];
-// // dailyRainTotals.map((hourlyRainTotals) => {
-// // 	return hourlyRainTotals.reduce((sum, inchesOfRain) => {
-// // 		return sum + inchesOfRain;
-// // 	});
-// // });
-// dailyRainTotals.map((hourlyRainTotals) =>
-// 	hourlyRainTotals.reduce((sum, inchesOfRain) => sum + inchesOfRain)
-// );
-
-// // const double = (n) => {
-// // 	return n * 2;
-// // };
-
-// // **********************
-// // Arrow Function Gotchas
-// // **********************
-// // const makeMath = (num) => {
-// // 	return {
-// // 		square : num * num,
-// // 		double : num * 2
-// // 	};
-// // };
-
-// // USE PARENS TO IMPLICITLY RETURN AN OBJECT:
-// const makeMath = (num) => ({
-// 	square : num * num,
-// 	double : num * 2
-// });
-
-// // THE KEYWORD 'THIS' BEHAVES DIFFERENTLY!
-// const cat = {
-// 	name : 'Bubs',
-// 	eat  : function() {
-// 		console.log(this);
-// 		return `${this.name} chows down`;
-// 	},
-// 	meow : () => {
-// 		console.log(this);
-// 		return `${this.name} says MEOW!!!`;
-// 	}
-// };
-
-
-
-
-
-
-// Array.map(function(x){
-// 	return x * 2
-// })
-
-
-// // regular function
-// const add = function(x, y) {
-// 	return x + y;
-// }; 
-// // arrow function
-// const add = (x,y) => {
-// 	return x + y;
-// }
-// // regular function
-// [1,2,3].forEach(function (n, idx){
-// 	console.log(n, idx);
-// });
-// // arrow function
-// [1,2,3].forEach((n, idx) => {
-// 	console.log(n, idx);
-// });
-// // regular function
-// [2,3,6,78,99,104,23].reduce(function(max, currNum){
-// 	return Math.max(max, currNum);
-// })
-// // arrow function
-// [2,3,6,78,99,104,23].reduce((max, currNum) => {
-// 	return Math.max(max, currNum);
-// })
-
-
-// SHORTCUTS
-
-// [1,2,3,4,5].forEach((n) => {
-// 	console.log(n * 10);
-// })
-
-// const greet = () => {
-// 	console.log('HELLO!')
-// };
 
 // regular function
-// [1,2,3,4,5,6].filter(function(num){
-// 	return num % 2 === 0;
-// });
-// // arrow shortcut
-// [1,2,3,4,5,6].filter((num) => num % 2 === 0);
-
-
-// const double = (n) => n * 2;
-
-// // regular function 
-// [1,5,7,9,10,12,13,15].map((n) => {
-// 	if (n % 2 === 0) {
-// 		return 'even';
-// 	}
-// 	return 'odd';
-// });
-// // arrow shortcut
-// [1,5,7,9,10,12,13,15].map((n) => (n % 2 === 0 ? 'even' : 'odd'));
-
-const dailyRainTotals = [ 
-	[1.2, 0.35, 2.2], 
-	[1.7, 0.6, 0.1], 
-	[2.5, 0.9, 1.5]
-];
-// regular function
-dailyRainTotals.map((hourlyRainTotals) => {
-	return hourlyRainTotals.reduce((sum, inchesOfRain) => {
-		return sum + inchesOfRain;
+function double(arr) {
+	return arr.map(function(val) {
+	  return val * 2;
 	});
-});
-// arrow shortcut 
-dailyRainTotals.map((hourlyRainTotals) => hourlyRainTotals.reduce((sum, inchesOfRain) => sum + inchesOfRain));
+  }
+// arrow function 
+// const double = (arr) => val * 2;
+const double = arr => arr.map(val => val * 2);
 
-// const double = (n) => {
-// 	return n * 2;
-// };
 
+// regular function 
+function squareAndFindEvens(numbers){
+	var squares = numbers.map(function(num){
+	  return num ** 2;
+	});
+	var evens = squares.filter(function(square){
+	  return square % 2 === 0;
+	});
+	return evens;
+  }
+
+//   arrow function 
+// const squareAndFindEvens = numbers => numbers.map((num => num ** 2) => squares.filter((square => square % 2 === 0 : 'evens'));
+const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0)
